a2d0b39a82e2e54a045b46c8d0e26a5ac10e2879
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-14 14:54:19 +0100

    feat: add structure js palindrome-word folder

--- Palindrome-word/js/script.js
+++ Palindrome-word/js/script.js
@@ -2,4 +2,9 @@
 
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
-// Su internet ci sono soluzioni con join e reverse, non potete usarle!
+
+// 1 - Chiedo all'utente una parola
+
+// 2 - Creo una funzione per "leggere" la parola dell'utente al contrario
+    // - se la parola inserita dall'utente e quella al contrario della funzione sono uguali allora la parola è palindroma
+    // - altrimenti se sono diverse la parola non è palindroma
