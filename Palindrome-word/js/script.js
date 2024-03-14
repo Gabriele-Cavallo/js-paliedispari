18fce5cdb040fe5aa8756009ba3194276be0f9ff
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-14 14:54:40 +0100

    feat: add user prompt

--- Palindrome-word/js/script.js
+++ Palindrome-word/js/script.js
@@ -4,7 +4,8 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // 1 - Chiedo all'utente una parola
-
+let userWord = prompt('Dimmi una parola');
+console.log('user-word' , userWord);
 // 2 - Creo una funzione per "leggere" la parola dell'utente al contrario
     // - se la parola inserita dall'utente e quella al contrario della funzione sono uguali allora la parola è palindroma
     // - altrimenti se sono diverse la parola non è palindroma
