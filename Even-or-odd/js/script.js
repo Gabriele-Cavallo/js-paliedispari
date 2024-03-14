000c6f8a12c3e9f1180908e85b4f5da5c33bf642
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-14 13:47:26 +0100

    feat: add computer random number function

--- Even-or-odd/js/script.js
+++ Even-or-odd/js/script.js
@@ -12,7 +12,9 @@ console.log('user-even-or-odd' , userEvenOrOdd);
 // 2 - Chiedo all'utente un numero da 1 a 5
 let userNumber = parseFloat(prompt('Dimmi un numero da 1 a 5'));
 console.log('user-number' , userNumber);
-// 3 - Genero un numero random da 1 a 5 per il computer
+// 3 - Genero un numero random da 1 a 5 per il computer (usando una funzione).
+let randomComputerNumber = getRndInteger(1, 5)
+console.log('random-computer-number' , randomComputerNumber);
 // 4 - Sommo i numeri di utente e computer
 // 5 - Creo una funzione per stabilire se la somma dei due numeri è pari o dispari
 // 6 - Dichiaro il vincitore
@@ -20,3 +22,11 @@ console.log('user-number' , userNumber);
             // - altrimenti vince il computer
         // - Se la somma è dispari e il giocatore ha scelto dispari vince
             // - altrimenti vince il computer
+
+//  FUNCTIONS
+
+// Random cumputer number
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
