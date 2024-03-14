582b93e7dbcbe5284950f8426367b9bce81f7c54
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-03-14 13:39:23 +0100

    feat: add structure js even-or-odd

--- Even-or-odd/js/script.js
+++ Even-or-odd/js/script.js
@@ -5,3 +5,14 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+// 1 - Chiedo all'utente di scegliere pari o dispari
+// 2 - Chiedo all'utente un numero da 1 a 5
+// 3 - Genero un numero random da 1 a 5 per il computer
+// 4 - Sommo i numeri di utente e computer
+// 5 - Creo una funzione per stabilire se la somma dei due numeri è pari o dispari
+// 6 - Dichiaro il vincitore
+        // - Se il giocatore ha scelto pari e la somma è pari vince il giocatore
+            // - altrimenti vince il computer
+        // - Se la somma è dispari e il giocatore ha scelto dispari vince
+            // - altrimenti vince il computer
